217c756d4629969abf3108b710b1d8cca9f3ebca
Sigit Kuncoro <sigit.kuncoro@sebangsa.com>
2018-11-08 14:52:40 +0700

    build(Config): update babel & webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,9 @@ const moduleObj = {
   loaders: [
     {
       test: /\.(js|jsx)$/,
-      exclude: /(node_modules|bower_components)/,
+      exclude: [
+        path.resolve(__dirname, "node_modules"),
+      ],
       use: {
         loader: "babel-loader",
         options: {
